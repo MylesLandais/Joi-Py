70966cc612c6d9d9544ed8c450070591871400c1
MylesLandais <myles.landais@protonmail.com>
2024-03-23 13:15:30 -0500

    Adding dotenv for managing discord token

--- qwen.py
+++ qwen.py
@@ -3,18 +3,22 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
+from huggingface_hub import snapshot_download
 
-
-
+# Define Model Directory
+chkpt = "4bit/Qwen-VL-Chat-Int4"
+model_dir = snapshot_download(chkpt)
 # Load the model
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="auto", trust_remote_code=True).eval()
-model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True).eval()
+model.generation_config = GenerationConfig.from_pretrained(model_dir, trust_remote_code=True)
+
+# TODO: Test no img prompt~
 
 # Inference/Chat
 query = tokenizer.from_list_format([
-    {'image': 'https://www.google.com/logos/doodles/2024/international-womens-day-2024-6753651837110196-l.webp'},
-    {'text': 'What does the image say?'},
+    {'image':'https://cdn.discordapp.com/attachments/1212843143574192248/1215879742239281271/140945409.webp?ex=65fe5b40&is=65ebe640&hm=782b718f7bbefc622a237d9d37f580cd6cbfbe8c4775d63bf2119e758aca84b4&'},
+    {'text': 'Describe the Image?'},
 ])
 response, history = model.chat(tokenizer, query=query, history=None)
 print(response)
@@ -25,7 +29,7 @@ response_2, history = model.chat(tokenizer, query=query_2, history=history)
 print(response_2)
 print(history)
 query_3 = tokenizer.from_list_format([
-    {'text': 'What is the url for the domain?'}
+    {'text': 'Write comma seperated tags describing the img with specific keywords'}
 ])
 response_3, history = model.chat(tokenizer, query=query_3,history=history)
 print(response_3)
